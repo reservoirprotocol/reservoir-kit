642945c269cf5eed35d75ce975b0e65ab04b2afd
pedromcunha <pedro.spinsomewebs@gmail.com>
2022-07-12 16:10:08 -0400

    Export useCoreSdk hook from index

--- packages/ui/src/hooks/index.tsx
+++ packages/ui/src/hooks/index.tsx
@@ -1,3 +1,4 @@
 export { default as useCollection } from './useCollection'
 export { default as useTokenDetails } from './useTokenDetails'
 export { default as useEthConverter } from './useEthConverter'
+export { default as useCoreSdk } from './useCoreSdk'
