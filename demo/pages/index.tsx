7c3409f1d47597d335b133da6b0bb51744cf274f
Michael Morrissey <mikehmorrissey@gmail.com>
2022-07-28 10:45:27 -0700

    switch step immediately after clicking to buy

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -89,7 +89,7 @@ const Index: NextPage = () => {
           </div>
         }
         collectionId="0xf5de760f2e916647fd766b4ad9e85ff943ce3a2b"
-        tokenId="91523"
+        tokenId="754238"
         onGoToToken={() => console.log('Awesome!')}
         onComplete={() => console.log('Purchase Complete')}
       />
