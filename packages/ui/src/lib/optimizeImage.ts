93a71d9c38757e7b0216656fa11189200a6039d7
Michael Morrissey <mikehmorrissey@gmail.com>
2022-08-05 09:43:17 -0700

    remove comments

--- packages/ui/src/lib/optimizeImage.ts
+++ packages/ui/src/lib/optimizeImage.ts
@@ -1,24 +1,6 @@
-const optimizeImage = (
-  imageHref: string | undefined,
-  // innerWidth: any,
-  // obj: { sm: number; md: number; lg: number; xl: number; '2xl': number }
-  width: number
-) => {
+const optimizeImage = (imageHref: string | undefined, width: number) => {
   if (!imageHref) return ''
-
-  // const w =
-  //   innerWidth < 639
-  //     ? obj.sm
-  //     : innerWidth < 767
-  //     ? obj.md
-  //     : innerWidth < 1023
-  //     ? obj.lg
-  //     : innerWidth < 1279
-  //     ? obj.xl
-  //     : obj['2xl']
-
   let url = new URL(imageHref)
-  // Optimize google images
   if (url.host === 'lh3.googleusercontent.com') {
     if (imageHref.includes('=s') || imageHref.includes('=w')) {
       let newImage = imageHref.split('=')
