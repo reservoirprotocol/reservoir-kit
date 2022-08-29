0a208f7524d21f9190ebe9cec856587edcd8ddde
pedromcunha <pedro.spinsomewebs@gmail.com>
2022-08-29 13:12:59 -0400

    Rename TokenOffer to Bid

--- packages/ui/src/modal/bid/TransactionBidDetails.tsx
+++ packages/ui/src/modal/bid/TransactionBidDetails.tsx
@@ -2,7 +2,7 @@ import React, { FC, useEffect, useState } from 'react'
 import { Flex, Box, FormatWEth, Text } from '../../primitives'
 import TokenStatsHeader from './TokenStatsHeader'
 import { useTokenDetails, useCollection } from '../../hooks'
-import { BidData } from './TokenOfferModalRenderer'
+import { BidData } from './BidModalRenderer'
 import { useTimeSince } from '../../hooks'
 import { formatEther } from 'ethers/lib/utils'
 
