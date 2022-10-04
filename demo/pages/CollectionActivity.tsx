a34b586328c4759e5b621b6bcc2234a4cbec03e3
Diego Ramos <ramosdiego@pm.me>
2022-10-04 11:41:38 -0400

    added `collectionActivity` endpoint hook

---
+++ demo/pages/CollectionActivity.tsx
@@ -0,0 +1,62 @@
+import { NextPage } from 'next'
+import { useCollectionActivity } from '@reservoir0x/reservoir-kit-ui'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useInView } from 'react-intersection-observer'
+import { useEffect } from 'react'
+
+const Activity: NextPage = () => {
+  const {
+    data: tokens,
+    fetchNextPage,
+    hasNextPage,
+  } = useCollectionActivity('0x731b0115fec5b8ee06d98565a7b4b663cad086ba')
+
+  const { ref, inView } = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage()
+    }
+  }, [inView])
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+        gap: 12,
+        padding: 24,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+      }}
+    >
+      <ConnectButton />
+      <h3 style={{ fontSize: 20, fontWeight: 600 }}>Tokens</h3>
+      {tokens.map((token, i) => (
+        <div key={`${token.token.tokenId}-${i}`}>
+          <div>Id: {token.token.tokenId}</div>
+          <div>Name: {token.token.name}</div>
+        </div>
+      ))}
+      {hasNextPage ? (
+        <div
+          style={{
+            fontWeight: 600,
+            fontSize: 16,
+            padding: 10,
+            width: '100%',
+            flexShrink: 0,
+          }}
+          ref={ref}
+        >
+          Loading
+        </div>
+      ) : (
+        <div>No more data</div>
+      )}
+    </div>
+  )
+}
+
+export default Tokens
---
+++ packages/ui/src/hooks/useCollectionActivity.ts
@@ -0,0 +1,73 @@
+import { paths, setParams } from '@reservoir0x/reservoir-kit-client'
+import useReservoirClient from './useReservoirClient'
+import useSWRInfinite, { SWRInfiniteConfiguration } from 'swr/infinite'
+
+type CollectionActivityResponse =
+  paths['/collections/{collection}/activity/v2']['get']['responses']['200']['schema']
+
+type CollectionActivityQuery =
+  paths['/collections/{collection}/activity/v2']['get']['parameters']['query']
+
+export default function (
+  collection?: string | undefined,
+  options?: CollectionActivityQuery | false,
+  swrOptions: SWRInfiniteConfiguration = {}
+) {
+  const client = useReservoirClient()
+
+  const { data, mutate, error, isValidating, size, setSize } =
+    useSWRInfinite<CollectionActivityResponse>(
+      (pageIndex, previousPageData) => {
+        if (!collection) {
+          return null
+        }
+
+        const url = new URL(
+          `${client?.apiBase}/collections/${collection}/activity/v2`
+        )
+
+        let query: CollectionActivityQuery = { ...options }
+
+        if (previousPageData && previousPageData.activities?.length === 0) {
+          return null
+        } else if (previousPageData && pageIndex > 0) {
+          query.continuation = previousPageData.continuation
+        }
+
+        setParams(url, query)
+
+        return [url.href, client?.apiKey, client?.version]
+      },
+      null,
+      {
+        revalidateOnMount: true,
+        revalidateFirstPage: false,
+        ...swrOptions,
+      }
+    )
+
+  const activities = data?.flatMap((page) => page.activities) ?? []
+  const lastPageTokenCount = data?.[size - 1]?.activities?.length || 0
+  const isFetchingInitialData = !data && !error
+  const isFetchingPage =
+    isFetchingInitialData ||
+    (size > 0 && data && typeof data[size - 1] === 'undefined')
+  const hasNextPage = lastPageTokenCount > 0 || isFetchingPage
+  const fetchNextPage = () => {
+    if (!isFetchingPage && hasNextPage) {
+      setSize((size) => size + 1)
+    }
+  }
+
+  return {
+    response: data,
+    data: activities,
+    hasNextPage,
+    isFetchingInitialData,
+    isFetchingPage,
+    fetchNextPage,
+    mutate,
+    error,
+    isValidating,
+  }
+}
