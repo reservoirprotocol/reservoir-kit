7d969ee8951ad7bfdd245afec246d9df62acacc0
r3lays <partscii@gmail.com>
2023-03-01 17:55:22 -0800

    Add types

--- packages/ui/src/hooks/useTokenActivity.ts
+++ packages/ui/src/hooks/useTokenActivity.ts
@@ -1,5 +1,12 @@
+import { paths, setParams } from '@reservoir0x/reservoir-sdk'
+import { SWRInfiniteConfiguration } from 'swr/infinite'
+import { useInfiniteApi, useReservoirClient } from './'
 
+type TokenActivityQuery =
+  paths['/tokens/{token}/activity/v4']['get']['parameters']
+type TokenActivityResponse =
+  paths['/tokens/{token}/activity/v4']['get']['responses']['200']['schema']
 
-export default function(): void {
-
-};
+export default function (options: TokenActivityQuery | false): void {
+  const client = useReservoirClient()
+}
