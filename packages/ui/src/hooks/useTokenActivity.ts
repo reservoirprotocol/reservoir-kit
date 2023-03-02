3928e247c3437c0dc59c49dce971236815ce7cc2
r3lays <partscii@gmail.com>
2023-03-01 18:00:34 -0800

    More base functionality

--- packages/ui/src/hooks/useTokenActivity.ts
+++ packages/ui/src/hooks/useTokenActivity.ts
@@ -3,10 +3,57 @@ import { SWRInfiniteConfiguration } from 'swr/infinite'
 import { useInfiniteApi, useReservoirClient } from './'
 
 type TokenActivityQuery =
-  paths['/tokens/{token}/activity/v4']['get']['parameters']
+  paths['/tokens/{token}/activity/v4']['get']['parameters']['query']
+
 type TokenActivityResponse =
   paths['/tokens/{token}/activity/v4']['get']['responses']['200']['schema']
 
-export default function (options: TokenActivityQuery | false): void {
+export default function (
+  options: TokenActivityQuery | false,
+  swrOptions: SWRInfiniteConfiguration = {},
+  chainId?: number
+) {
   const client = useReservoirClient()
+  const chain =
+    chainId !== undefined
+      ? client?.chains.find((chain) => chain.id === chainId)
+      : client?.currentChain()
+
+  const response = useInfiniteApi<TokenActivityResponse>(
+    (pageIndex, previousPageData) => {
+      if (
+        !options ||
+        (!options.collection && !options.collectionsSetId && !options.community)
+      ) {
+        return null
+      }
+
+      const url = new URL(`${chain?.baseApiUrl}/collections/activity/v5`)
+
+      let query: TokenActivityQuery = { ...options }
+
+      if (previousPageData && !previousPageData.continuation) {
+        return null
+      } else if (previousPageData && pageIndex > 0) {
+        query.continuation = previousPageData.continuation
+      }
+
+      setParams(url, query)
+
+      return [url.href, chain?.apiKey, client?.version]
+    },
+    {
+      revalidateOnMount: true,
+      revalidateFirstPage: false,
+      ...swrOptions,
+    }
+  )
+
+  const activities =
+    response.data?.flatMap((page) => page.activities || []) ?? []
+
+  return {
+    ...response,
+    data: activities,
+  }
 }
