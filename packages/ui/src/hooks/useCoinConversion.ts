3ede52f7df91384bcdf035ebed57598903ecb678
r3lays <partscii@gmail.com>
2023-03-01 17:37:47 -0800

    Revert changes in branch

--- packages/ui/src/hooks/useCoinConversion.ts
+++ packages/ui/src/hooks/useCoinConversion.ts
@@ -1,27 +1,13 @@
-import { useContext } from 'react'
 import useSWR from 'swr'
-import { CoinGecko, ProviderOptionsContext } from '../ReservoirKitProvider'
 
-const createBaseUrl = (config: CoinGecko | undefined): string => {
-  if (config?.proxyUrl) {
-    return `${config.proxyUrl}?`
-  }
-
-  if (config?.apiKey) {
-    return `https://pro-api.coingecko.com/api/v3/coins/markets?x_cg_pro_api_key={${config.apiKey}}&`
-  }
-
-  return `https://api.coingecko.com/api/v3/coins/markets?`
-}
-
-export default function (vs_currency?: string, symbols: string = 'eth', id: string = '') {
-  const { coinGecko } = useContext(ProviderOptionsContext)
-
-  const baseUrl = createBaseUrl(coinGecko)
-
+export default function (
+  vs_currency?: string,
+  symbols: string = 'eth',
+  id: string = ''
+) {
   const { data } = useSWR(
     vs_currency
-      ? `${baseUrl}vs_currency=${vs_currency}&symbols=${symbols}&ids=${id}`
+      ? `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${vs_currency}&symbols=${symbols}&ids=${id}`
       : null,
     null,
     {
