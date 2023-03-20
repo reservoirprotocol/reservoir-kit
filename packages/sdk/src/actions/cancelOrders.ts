5952721ddad2233dff496fb0e292c49f6d7e44da
Chris Jacobs <chris@originprotocol.com>
2023-03-20 15:00:55 -0700

    create `cancelOrders.ts`

---
+++ packages/sdk/src/actions/cancelOrders.ts
@@ -0,0 +1,56 @@
+import { Execute, paths } from '../types'
+import { Signer } from 'ethers'
+import { executeSteps } from '../utils'
+import { getClient } from '.'
+
+type CancelOrdersPathParameters =
+  paths['/execute/cancel/v3']['post']['parameters']['body']
+
+export type CancelOrdersOptions = Omit<NonNullable<CancelOrdersPathParameters['body']>, 'params'>
+
+type Data = {
+  orderIds: string[]
+  signer: Signer
+  options?: CancelOrdersOptions
+  onProgress: (steps: Execute['steps']) => any
+}
+
+/**
+ * Cancel an offer or listing
+ * @param data.id Id of the order to cancel
+ * @param data.signer Ethereum signer object provided by the browser
+ * @param data.options Additional options to pass into the cancel request
+ * @param data.onProgress Callback to update UI state has execution progresses
+ */
+export async function cancelOrders(data: Data) {
+  const { orderIds, signer, onProgress } = data
+  const client = getClient()
+  const options = data.options || {}
+  const baseApiUrl = client.currentChain()?.baseApiUrl
+
+  if (!baseApiUrl) {
+    throw new ReferenceError('ReservoirClient missing chain configuration')
+  }
+
+  try {
+    await executeSteps(
+      {
+        method: 'post',
+        url: `${baseApiUrl}/execute/cancel/v3`,
+        data: {
+          params: {
+            kind: 'orderIds',
+            data: { orderIds }
+          },
+          ...options
+        }
+      },
+      signer,
+      onProgress
+    )
+    return true
+  } catch (err: any) {
+    console.error(err)
+    throw err
+  }
+}
