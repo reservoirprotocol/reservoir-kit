b67305367b632d56815042462ff5491868c49c61
Ted Palmer <etpalmer17@gmail.com>
2023-03-21 19:10:11 -0400

    moved zoneAddresses to constants folder

--- packages/ui/src/modal/editListing/EditListingModalRenderer.tsx
+++ packages/ui/src/modal/editListing/EditListingModalRenderer.tsx
@@ -25,6 +25,7 @@ import dayjs from 'dayjs'
 import { constants } from 'ethers'
 import { Listings } from '../list/ListModalRenderer'
 import { formatUnits, parseUnits } from 'ethers/lib/utils.js'
+import zoneAddresses from '../../constants/zoneAddresses'
 
 export enum EditListingStep {
   Edit,
@@ -78,11 +79,6 @@ type Props = {
   enableOnChainRoyalties: boolean
   children: (props: ChildrenProps) => ReactNode
 }
-
-const zoneAddresses = [
-  '0xe1066481cc3b038badd0c68dfa5c8f163c3ff192', // Ethereum - 0xe1...92
-  '0x49b91d1d7b9896d28d370b75b92c2c78c1ac984a', // Goerli Address - 0x49...4a
-]
 
 export const EditListingModalRenderer: FC<Props> = ({
   open,
