3f7ec12b20414a0b866489ecba501ab8b590776c
Ted Palmer <etpalmer17@gmail.com>
2023-04-18 10:15:14 -0400

    Improved code formatting

--- packages/ui/src/modal/sweep/SweepModal.tsx
+++ packages/ui/src/modal/sweep/SweepModal.tsx
@@ -482,6 +482,7 @@ export function SweepModal({
                 </Flex>
               </Flex>
             )}
+
             {sweepStep === SweepStep.Complete && (
               <Flex
                 direction="column"
