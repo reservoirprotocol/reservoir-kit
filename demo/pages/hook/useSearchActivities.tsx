3d0d469d98b7ab96836b0f0ff1a51756a541c02e
armando <armandomvillanueva@icloud.com>
2023-05-23 09:56:01 -0700

    feat: useSearchActivities demo

---
+++ demo/pages/hook/useSearchActivities.tsx
@@ -0,0 +1,61 @@
+import { useSearchActivities } from '@reservoir0x/reservoir-kit-ui'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { NextPage } from 'next'
+import { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
+
+const Activity: NextPage = () => {
+  const {
+    data: activity,
+    fetchNextPage,
+    hasNextPage,
+  } = useSearchActivities({
+    collection: '0xed5af388653567af2f388e6224dc7c4b3241c544',
+  })
+
+  const { ref, inView } = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage()
+    }
+  }, [inView])
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+        gap: 12,
+        padding: 24,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+      }}
+    >
+      <ConnectButton />
+      <h3 style={{ fontSize: 20, fontWeight: 600 }}>Activity</h3>
+      {activity.map((token, i) => (
+        <pre>{JSON.stringify(token, null, 2)}</pre>
+      ))}
+      {hasNextPage ? (
+        <div
+          style={{
+            fontWeight: 600,
+            fontSize: 16,
+            padding: 10,
+            width: '100%',
+            flexShrink: 0,
+          }}
+          ref={ref}
+        >
+          Loading
+        </div>
+      ) : (
+        <div>No more data</div>
+      )}
+    </div>
+  )
+}
+
+export default Activity
