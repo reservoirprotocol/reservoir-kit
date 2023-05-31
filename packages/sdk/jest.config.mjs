7ba6c48d4b673162c2aa89b458e67c3d3185cfb0
armando <armandomvillanueva@icloud.com>
2023-05-31 07:09:11 -0700

    feat: Fix conflicts and remerge jest

---
+++ packages/sdk/jest.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  testEnvironment: 'node', // Specifies that the test environment is Node.js
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // Uses the 'ts-jest' transformer for TypeScript files
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // Specifies the file extensions to be used for modules
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$', // Specifies the pattern used to find test files
+  coverageDirectory: 'coverage', // Specifies the directory where coverage reports will be saved
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'], // Specifies the files for which coverage reports will be collected, excluding declaration files
+}
