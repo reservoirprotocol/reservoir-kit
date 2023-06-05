9c6fc2203c524971a9decd1d5df1f83e1fcc3438
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-06-05 12:37:34 -0400

    chore: comment change

--- packages/ui/src/constants/wrappedContracts.ts
+++ packages/ui/src/constants/wrappedContracts.ts
@@ -4,7 +4,7 @@ const wrappedContracts: Record<number, string> = {
   10: '0x4200000000000000000000000000000000000006', //optimism
   137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', //polygon
   42161: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1', //arbitrum
-  999: '', //zora
+  999: '', //zoratestnet
   80001: '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889', //mumbai
   84531: '0x4200000000000000000000000000000000000006', //base goerli
   534353: '0xa1EA0B2354F5A344110af2b6AD68e75545009a03', //scroll
