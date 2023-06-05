745c17eaaeafb7c00a6c74a7406eab0583af954a
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-06-05 10:22:01 -0400

    add wrapped contract details for testnet chains

--- packages/ui/src/constants/wrappedContractNames.ts
+++ packages/ui/src/constants/wrappedContractNames.ts
@@ -1,8 +1,13 @@
 const wrappedContractNames: Record<number, string> = {
-  1: 'WETH',
-  5: 'WETH',
-  10: 'WETH',
-  137: 'WMATIC',
-  42161: 'WETH',
+  1: 'WETH', //mainnet
+  5: 'WETH', //goerli
+  10: 'WETH', //optimism
+  137: 'WMATIC', //polygon
+  42161: 'WETH', //arbitrum
+  999: 'WETH', //zoratestnet
+  80001: 'WETH', //mumbai
+  84531: 'WETH', //base goerli
+  534353: 'WETH', //scroll
+  11155111: 'WETH', //sepolia
 }
 export default wrappedContractNames
