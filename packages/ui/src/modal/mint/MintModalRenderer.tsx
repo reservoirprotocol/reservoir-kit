ce10aaa301ae080c866a1a509dfb3f854f91c202
Ted Palmer <etpalmer17@gmail.com>
2023-06-13 14:32:05 -0400

    wip: MintRenderer

---
+++ packages/ui/src/modal/mint/MintModalRenderer.tsx
@@ -0,0 +1,437 @@
+import React, {
+  FC,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useState,
+  useMemo,
+} from 'react'
+import {
+  useChainCurrency,
+  useCoinConversion,
+  useReservoirClient,
+  useTokens,
+} from '../../hooks'
+import { useAccount, useBalance, useNetwork, useWalletClient } from 'wagmi'
+import Token from '../list/Token'
+import {
+  BuyPath,
+  Execute,
+  ReservoirChain,
+  ReservoirClientActions,
+} from '@reservoir0x/reservoir-sdk'
+import { toFixed } from '../../lib/numbers'
+import { UseBalanceToken } from '../../types/wagmi'
+import { Address, formatUnits, parseUnits, zeroAddress } from 'viem'
+
+export enum MintStep {
+  Idle,
+  Approving,
+  Finalizing,
+  Complete,
+}
+
+export type MintModalStepData = {
+  totalSteps: number
+  stepProgress: number
+  currentStep: Execute['steps'][0]
+  currentStepItem: NonNullable<Execute['steps'][0]['items']>[0]
+  path: Execute['path']
+}
+
+type Token = ReturnType<typeof useTokens>['data'][0]
+
+type BuyTokenOptions = Parameters<
+  ReservoirClientActions['buyToken']
+>['0']['options']
+
+type Currency = NonNullable<
+  NonNullable<NonNullable<Token['market']>['floorAsk']>['price']
+>['currency']
+
+type ChildrenProps = {
+  loading: boolean
+  selectedTokens: NonNullable<BuyPath>
+  setSelectedTokens: React.Dispatch<React.SetStateAction<NonNullable<BuyPath>>>
+  itemAmount?: number
+  setItemAmount: React.Dispatch<React.SetStateAction<number | undefined>>
+  maxInput: number
+  setMaxInput: React.Dispatch<React.SetStateAction<number>>
+  currency: ReturnType<typeof useChainCurrency>
+  chainCurrency: ReturnType<typeof useChainCurrency>
+  isChainCurrency: boolean
+  total: number
+  totalUsd: number
+  feeOnTop: number
+  feeUsd: number
+  usdPrice: number
+  currentChain: ReservoirChain | null | undefined
+  availableTokens: BuyPath
+  address?: string
+  tokens: BuyPath
+  balance?: bigint
+  hasEnoughCurrency: boolean
+  blockExplorerBaseUrl: string
+  transactionError: Error | null | undefined
+  stepData: MintModalStepData | null
+  setStepData: React.Dispatch<React.SetStateAction<MintModalStepData | null>>
+  mintStep: MintStep
+  setMintStep: React.Dispatch<React.SetStateAction<MintStep>>
+  mintTokens: () => void
+}
+
+type Props = {
+  open: boolean
+  collectionId?: string
+  feesOnTopBps?: string[] | null
+  feesOnTopFixed?: string[] | null
+  normalizeRoyalties?: boolean
+  children: (props: ChildrenProps) => ReactNode
+}
+
+export const MintModalRenderer: FC<Props> = ({
+  open,
+  collectionId,
+  feesOnTopBps,
+  feesOnTopFixed,
+  normalizeRoyalties,
+  children,
+}) => {
+  const { data: signer } = useWalletClient()
+  const account = useAccount()
+  const [selectedTokens, setSelectedTokens] = useState<NonNullable<BuyPath>>([])
+  const [itemAmount, setItemAmount] = useState<number | undefined>(undefined)
+  const [maxInput, setMaxInput] = useState<number>(0)
+  const [mintStep, setMintStep] = useState<MintStep>(MintStep.Idle)
+  const [stepData, setStepData] = useState<MintModalStepData | null>(null)
+  const [transactionError, setTransactionError] = useState<Error | null>()
+
+  const [hasEnoughCurrency, setHasEnoughCurrency] = useState(true)
+  const [feeOnTop, setFeeOnTop] = useState(0)
+
+  const chainCurrency = useChainCurrency()
+  const [currency, setCurrency] = useState(chainCurrency)
+
+  const isChainCurrency = currency.address === chainCurrency.address
+
+  const client = useReservoirClient()
+  const currentChain = client?.currentChain()
+
+  const { chains } = useNetwork()
+  const chain = chains.find((chain) => chain.id === currentChain?.id)
+
+  const blockExplorerBaseUrl =
+    chain?.blockExplorers?.default?.url || 'https://etherscan.io'
+
+  const [fetchedInitialTokens, setFetchedInitialTokens] = useState(false)
+  const [tokens, setTokens] = useState<BuyPath | undefined>(undefined)
+
+  const fetchBuyPath = useCallback(() => {
+    if (!signer || !client) {
+      return
+    }
+
+    let options: BuyTokenOptions = {
+      partial: true,
+      onlyPath: true,
+    }
+
+    if (normalizeRoyalties !== undefined) {
+      options.normalizeRoyalties = normalizeRoyalties
+    }
+
+    client?.actions
+      .buyToken({
+        items: [
+          {
+            collection: collectionId,
+            fillType: 'mint',
+          },
+        ],
+        expectedPrice: undefined,
+        options,
+        signer: signer,
+        precheck: true,
+        onProgress: () => {},
+      })
+      .then((data) => {
+        setTokens(
+          'path' in (data as any)
+            ? ((data as Execute)['path'] as BuyPath)
+            : undefined
+        )
+      })
+      .finally(() => {
+        setFetchedInitialTokens(true)
+      })
+  }, [client, signer, normalizeRoyalties, collectionId, currency])
+
+  useEffect(() => {
+    if (open) {
+      fetchBuyPath()
+    }
+  }, [client, signer, open])
+
+  const total = useMemo(() => {
+    const updatedTotal = selectedTokens?.reduce((total, token) => {
+      return (
+        total +
+        (token?.currency != chainCurrency.address && isChainCurrency
+          ? token?.buyInQuote || 0
+          : token?.totalPrice || 0)
+      )
+    }, 0)
+
+    let fees = 0
+    if (feesOnTopBps && feesOnTopBps.length > 0) {
+      fees = feesOnTopBps.reduce((totalFees, feeOnTop) => {
+        const [_, fee] = feeOnTop.split(':')
+        return totalFees + (Number(fee) / 10000) * updatedTotal
+      }, 0)
+    } else if (feesOnTopFixed && feesOnTopFixed.length > 0) {
+      fees = feesOnTopFixed.reduce((totalFees, feeOnTop) => {
+        const [_, fee] = feeOnTop.split(':')
+        const parsedFee = formatUnits(BigInt(fee), currency?.decimals || 18)
+        return totalFees + Number(parsedFee)
+      }, 0)
+    }
+    setFeeOnTop(fees)
+
+    return updatedTotal + fees
+  }, [selectedTokens, feesOnTopBps, feesOnTopFixed, currency, isChainCurrency])
+
+  const coinConversion = useCoinConversion(
+    open ? 'USD' : undefined,
+    currency?.symbol
+  )
+  const usdPrice = coinConversion.length > 0 ? coinConversion[0].price : 0
+  const feeUsd = feeOnTop * usdPrice
+  const totalUsd = usdPrice * (total || 0)
+
+  const { data: balance } = useBalance({
+    chainId: chain?.id,
+    address: account.address,
+    token:
+      currency?.address !== zeroAddress
+        ? (currency?.address as UseBalanceToken)
+        : undefined,
+    watch: open,
+    formatUnits: currency?.decimals,
+  })
+
+  // Determine if user has enough funds
+  useEffect(() => {
+    if (balance) {
+      const totalPriceTruncated = toFixed(total, currency?.decimals || 18)
+      if (!balance.value) {
+        setHasEnoughCurrency(false)
+      } else if (
+        balance?.value <
+        parseUnits(`${totalPriceTruncated as number}`, currency?.decimals || 18)
+      ) {
+        setHasEnoughCurrency(false)
+      } else {
+        setHasEnoughCurrency(true)
+      }
+    }
+  }, [total, balance, currency])
+
+  const availableTokens = useMemo(() => {
+    if (!tokens) return []
+    return tokens
+  }, [tokens, account])
+
+  useEffect(() => {
+    setItemAmount(1)
+  }, [availableTokens.length])
+
+  // reset state on close
+  useEffect(() => {
+    if (!open) {
+      setSelectedTokens([])
+      setTokens(undefined)
+      setItemAmount(undefined)
+      setMintStep(MintStep.Idle)
+      setTransactionError(null)
+      setFetchedInitialTokens(false)
+    }
+  }, [open])
+
+  const mintTokens = useCallback(() => {
+    if (!signer) {
+      const error = new Error('Missing a signer')
+      setTransactionError(error)
+      throw error
+    }
+
+    if (!client) {
+      const error = new Error('ReservoirClient was not initialized')
+      setTransactionError(error)
+      throw error
+    }
+
+    setTransactionError(null)
+
+    let options: BuyTokenOptions = {
+      partial: true,
+    }
+
+    if (feesOnTopBps && feesOnTopBps?.length > 0) {
+      const fixedFees = feesOnTopBps.map((fullFee) => {
+        const [referrer, feeBps] = fullFee.split(':')
+        const totalFeeTruncated = toFixed(
+          total - feeOnTop,
+          currency?.decimals || 18
+        )
+        const fee =
+          Number(
+            parseUnits(
+              `${Number(totalFeeTruncated)}`,
+              currency?.decimals || 18
+            ) * BigInt(feeBps)
+          ) / 10000
+        const atomicUnitsFee = formatUnits(BigInt(fee), 0)
+        return `${referrer}:${atomicUnitsFee}`
+      })
+      options.feesOnTop = fixedFees
+    }
+    if (feesOnTopFixed && feesOnTopFixed.length > 0) {
+      options.feesOnTop = feesOnTopFixed
+    } else if (!feesOnTopFixed && !feesOnTopBps) {
+      delete options.feesOnTop
+    }
+
+    if (normalizeRoyalties !== undefined) {
+      options.normalizeRoyalties = normalizeRoyalties
+    }
+
+    if (!selectedTokens || selectedTokens.length === 0) {
+      const error = new Error('No tokens to mint')
+      setTransactionError(error)
+      throw error
+    }
+
+    setMintStep(MintStep.Approving)
+
+    client.actions
+      .buyToken({
+        items: [
+          {
+            collection: collectionId,
+            quantity: selectedTokens.length,
+            fillType: 'mint',
+          },
+        ],
+        expectedPrice: total - feeOnTop,
+        signer,
+        options,
+        onProgress: (steps: Execute['steps'], path: Execute['path']) => {
+          if (!steps) {
+            return
+          }
+
+          const executableSteps = steps.filter(
+            (step) => step.items && step.items.length > 0
+          )
+
+          let stepCount = executableSteps.length
+
+          let currentStepItem:
+            | NonNullable<Execute['steps'][0]['items']>[0]
+            | undefined
+
+          const currentStepIndex = executableSteps.findIndex((step) => {
+            currentStepItem = step.items?.find(
+              (item) => item.status === 'incomplete'
+            )
+            return currentStepItem
+          })
+
+          const currentStep =
+            currentStepIndex > -1
+              ? executableSteps[currentStepIndex]
+              : executableSteps[stepCount - 1]
+
+          if (currentStepItem) {
+            setStepData({
+              totalSteps: stepCount,
+              stepProgress: currentStepIndex,
+              currentStep,
+              currentStepItem,
+              path: path,
+            })
+          }
+
+          if (currentStep.items?.every((item) => item.txHash)) {
+            setMintStep(MintStep.Finalizing)
+          }
+
+          if (
+            steps.every(
+              (step) =>
+                !step.items ||
+                step.items.length == 0 ||
+                step.items?.every((item) => item.status === 'complete')
+            )
+          ) {
+            setMintStep(MintStep.Complete)
+          }
+        },
+      })
+      .catch((e: any) => {
+        const error = e as Error
+        const transactionError = new Error(error?.message || '', {
+          cause: error,
+        })
+        setTransactionError(transactionError)
+        setMintStep(MintStep.Idle)
+        fetchBuyPath()
+      })
+  }, [
+    selectedTokens,
+    client,
+    signer,
+    total,
+    normalizeRoyalties,
+    chain,
+    collectionId,
+    currency,
+    feesOnTopBps,
+    feesOnTopFixed,
+  ])
+
+  return (
+    <>
+      {children({
+        loading: !fetchedInitialTokens,
+        address: account?.address,
+        selectedTokens,
+        setSelectedTokens,
+        itemAmount,
+        setItemAmount,
+        maxInput,
+        setMaxInput,
+        currency,
+        chainCurrency,
+        isChainCurrency,
+        total,
+        totalUsd,
+        feeOnTop,
+        feeUsd,
+        usdPrice,
+        currentChain,
+        availableTokens,
+        tokens,
+        balance: balance?.value,
+        hasEnoughCurrency,
+        blockExplorerBaseUrl,
+        transactionError,
+        stepData,
+        setStepData,
+        mintStep,
+        setMintStep,
+        mintTokens,
+      })}
+    </>
+  )
+}
