958a694ff301348ba8e0ec710530f2c221b617fa
armando <armandomvillanueva@icloud.com>
2023-08-11 11:34:56 -0600

    feat: Define all event types

---
+++ packages/sdk/src/types/websocket.ts
@@ -0,0 +1,22 @@
+export interface WebSocketAskCreatedPayload {}
+export interface WebSocketAskUpdatedPayload {}
+
+export interface WebSocketBidCreatedPayload {}
+export interface WebSocketBidUpdatedPayload {}
+
+export interface WebSocketSaleCreatedPayload {}
+export interface WebSocketSaleUpdatedPayload {}
+
+export interface WebSocketTransferCreatedPayload {}
+export interface WebSocketTransferUpdatedPayload {}
+
+export interface WebSocketTokenCreatedPayload {}
+export interface WebSocketTokenUpdatedPayload {}
+
+export interface WebSocketTokenAttributeCreatedPayload {}
+export interface WebSocketTokenAttributeDeletedPayload {}
+
+export interface WebSocketCollectionCreatedPayload {}
+export interface WebSocketCollectionDeletedPayload {}
+
+export interface WebSocketTopBidChangedPayload {}
