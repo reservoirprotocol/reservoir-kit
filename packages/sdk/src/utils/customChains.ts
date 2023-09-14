b5cdaf47e1464a19e82fddc7e04259e055a11a69
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-09-14 14:42:58 -0400

    Remove unnecessary customChains that are now included in viem/wagmi

--- packages/sdk/src/utils/customChains.ts
+++ packages/sdk/src/utils/customChains.ts
@@ -1,81 +1,5 @@
 import { Chain } from "viem";
 
-export const zora = {
-  id: 7777777,
-  name: 'Zora',
-  network: 'zora',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: ['https://rpc.zora.co'],
-    },
-    public: {
-      http: ['https://rpc.zora.co'],
-    },
-  },
-  blockExplorers: {
-    etherscan: {
-      name: 'Zora explorer',
-      url: 'https://explorer.zora.co',
-    },
-    default: {
-      name: 'Zora explorer',
-      url: 'https://explorer.zora.co',
-    },
-  },
-} as const satisfies Chain
-
-export const zoraTestnet = {
-  id: 999,
-  name: 'Zora Testnet',
-  network: 'zora-testnet',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: ['https://testnet.rpc.zora.co'],
-    },
-    public: {
-      http: ['https://testnet.rpc.zora.co'],
-    },
-  },
-  blockExplorers: {
-    etherscan: {
-      name: 'Zora Testnet explorer',
-      url: 'https://testnet.explorer.zora.co',
-    },
-    default: {
-      name: 'Zora Testnet explorer',
-      url: 'https://testnet.explorer.zora.co',
-    },
-  },
-} as const satisfies Chain
-
-
-export const base = {
-  id: 8453,
-  name: 'Base',
-  network: 'base',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: ['https://developer-access-mainnet.base.org'],
-    },
-    public: {
-      http: ['https://developer-access-mainnet.base.org'],
-    },
-  },
-  blockExplorers: {
-    etherscan: {
-      name: 'Basescan',
-      url: 'https://basescan.org',
-    },
-    default: {
-      name: 'BaseScan',
-      url: 'https://basescan.org',
-    },
-  },
-} as const satisfies Chain
-
 export const arbitrumNova = {
   id: 42170,
   name: 'Arbitrum Nova',
@@ -109,38 +33,6 @@ export const arbitrumNova = {
   },
 } as const satisfies Chain
 
-export const linea = {
-  id: 59144,
-  name: 'Linea',
-  network: 'linea',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: ['https://rpc.linea.build'],
-    },
-    public: {
-      http: ['https://rpc.linea.build'],
-    },
-    infura: {
-      http: ['https://linea-mainnet.infura.io/v3']
-    }
-  },
-  blockExplorers: {
-    etherscan: {
-      name: 'Linea Explorer',
-      url: 'https://explorer.linea.build',
-    },
-    default: {
-      name: 'Linea Explorer',
-      url: 'https://explorer.linea.build',
-    },
-  },
-} as const satisfies Chain
-
 export const customChains = {
-  zora,
-  zoraTestnet,
-  base,
   arbitrumNova,
-  linea
 } as const satisfies Record<string, Chain>
