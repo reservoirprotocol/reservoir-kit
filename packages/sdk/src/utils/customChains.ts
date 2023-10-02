55dfc4525a38e762f16e02d983b103047b94589a
pedromcunha <pedro.spinsomewebs@gmail.com>
2023-10-02 11:08:31 -0400

    Add ancient8 chain

--- packages/sdk/src/utils/customChains.ts
+++ packages/sdk/src/utils/customChains.ts
@@ -33,6 +33,34 @@ export const arbitrumNova = {
   },
 } as const satisfies Chain
 
+export const ancient8Testnet = {
+  id: 2863311531,
+  name: 'Ancient8 Testnet',
+  network: 'ancient8',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-testnet.ancient8.gg'],
+      webSocket: ['https://rpc-testnet.ancient8.gg'],
+    },
+    public: {
+      http: ['https://rpc-testnet.ancient8.gg'],
+      webSocket: ['https://rpc-testnet.ancient8.gg'],
+    },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'a8scan',
+      url: 'https://testnet.a8scan.io/',
+    },
+    default: {
+      name: 'a8scan',
+      url: 'https://testnet.a8scan.io/',
+    },
+  },
+} as const satisfies Chain
+
 export const customChains = {
   arbitrumNova,
+  ancient8Testnet
 } as const satisfies Record<string, Chain>
