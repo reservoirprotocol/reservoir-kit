cb23f73ebc5a93cd4b12e6bc7f901059772c3a4d
Ted Palmer <etpalmer17@gmail.com>
2023-10-03 13:20:32 -0400

    Reset maxPollingAttempts to default

--- demo/pages/_app.tsx
+++ demo/pages/_app.tsx
@@ -100,7 +100,6 @@ const AppWrapper: FC<any> = ({ children }) => {
     <WagmiConfig config={wagmiConfig}>
       <ReservoirKitProvider
         options={{
-          maxPollingAttemptsBeforeTimeout: 0,
           chains: [
             {
               baseApiUrl: 'https://api.reservoir.tools',
