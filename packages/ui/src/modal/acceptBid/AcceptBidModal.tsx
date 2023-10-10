5ab1580734376470efca3e69642811c70a7d69dc
Ted Palmer <etpalmer17@gmail.com>
2023-10-10 15:50:09 -0400

    Improve AcceptBidModal's unavailable ui

--- packages/ui/src/modal/acceptBid/AcceptBidModal.tsx
+++ packages/ui/src/modal/acceptBid/AcceptBidModal.tsx
@@ -190,24 +190,25 @@ export function AcceptBidModal({
             loading={loading}
           >
             {acceptBidStep === AcceptBidStep.Unavailable && !loading && (
-              <Flex direction="column" css={{ mt: 80 }}>
-                <Box css={{ color: '$neutralSolidHover', m: '0 auto' }}>
+              <Flex
+                direction="column"
+                align="center"
+                css={{ p: '$4', gap: '$5' }}
+              >
+                <Box css={{ color: '$neutralSolid', mt: 48 }}>
                   <FontAwesomeIcon
                     icon={faEnvelopeOpen}
-                    style={{ height: 24, width: 24 }}
+                    style={{ height: 32, width: 32 }}
                   />
                 </Box>
-                <Text
-                  style="body2"
-                  css={{ mt: '$2', mb: 74, textAlign: 'center' }}
-                >
-                  Offers are no longer available
+                <Text style="h6" css={{ mb: '$3', textAlign: 'center' }}>
+                  {tokens.length > 1 ? 'Offers are ' : 'Offer is '} no longer
+                  available
                 </Text>
                 <Button
                   onClick={() => setOpen(false)}
                   css={{
-                    m: '$4',
-                    flex: 1,
+                    width: '100%',
                   }}
                 >
                   {copy.ctaClose}
