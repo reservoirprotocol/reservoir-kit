1410c240240ce4c12c420f64c6094c75ec4cc4d5
armando <armandomvillanueva@icloud.com>
2023-11-14 10:35:56 -0800

    fix: useSWR hook

--- packages/ui/src/hooks/useTrendingMints.ts
+++ packages/ui/src/hooks/useTrendingMints.ts
@@ -1,51 +1,39 @@
-import { paths, setParams } from "@reservoir0x/reservoir-sdk";
-import { useMemo } from "react";
-import { SWRInfiniteConfiguration } from "swr/infinite";
-import { useInfiniteApi, useReservoirClient } from "./";
+import { paths, setParams } from '@reservoir0x/reservoir-sdk'
+import useSWR, { SWRConfiguration } from 'swr'
+import { useReservoirClient } from './'
 
 type TrendingMintsResponse =
-  paths["/collections/trending-mints/v1"]["get"]["responses"]["200"]["schema"];
+  paths['/collections/trending-mints/v1']['get']['responses']['200']['schema']
 
 type TrendingMintsQuery =
-  paths["/collections/trending-mints/v1"]["get"]["parameters"]["query"];
+  paths['/collections/trending-mints/v1']['get']['parameters']['query']
 
 export default function (
-  options?: TrendingMintsQuery | false,
-  swrOptions: SWRInfiniteConfiguration = {},
-  chainId?: number
+  options: TrendingMintsQuery | false,
+  chainId?: number,
+  swrOptions: SWRConfiguration = {}
 ) {
-  const client = useReservoirClient();
+  const client = useReservoirClient()
   const chain =
     chainId !== undefined
       ? client?.chains.find((chain) => chain.id === chainId)
-      : client?.currentChain();
+      : client?.currentChain()
 
-  const response = useInfiniteApi<TrendingMintsResponse>(
-    (pageIndex, previousPageData) => {
-      if (!options) {
-        return null;
-      }
+  const url = new URL(`${chain?.baseApiUrl}/collections/trending-mints/v1`)
+  let query: TrendingMintsQuery = { ...options }
 
-      const url = new URL(`${chain?.baseApiUrl}/collections/trending-mints/v1`);
-      let query: TrendingMintsQuery = { ...options };
+  setParams(url, query)
 
-      setParams(url, query);
-      return [url.href, client?.apiKey, client?.version];
-    },
+  const { data, mutate, error, isValidating } = useSWR<TrendingMintsResponse>(
+    url && options ? [url.href, client?.apiKey, client?.version] : null,
+    null,
     {
       revalidateOnMount: true,
-      revalidateFirstPage: false,
       ...swrOptions,
     }
-  );
+  )
 
-  const mints = useMemo(
-    () => response.data?.flatMap((page) => page.mints || []) ?? [],
-    [response.data]
-  );
-
-  return {
-    ...response,
-    data: mints,
-  };
+  const mints: TrendingMintsResponse['mints'] | null =
+    data && data.mints ? data.mints : null
+  return { response: data, data: mints, mutate, error, isValidating }
 }
