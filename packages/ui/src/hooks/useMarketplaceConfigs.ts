0993e7c594e025d94e9e08ca6c860ed9c31cd882
Ted Palmer <etpalmer17@gmail.com>
2024-01-10 11:33:23 -0500

    chore: update useMarketplaceConfigs hook

--- packages/ui/src/hooks/useMarketplaceConfigs.ts
+++ packages/ui/src/hooks/useMarketplaceConfigs.ts
@@ -1,14 +1,21 @@
-import { ReservoirChain, paths } from '@reservoir0x/reservoir-sdk'
+import { paths } from '@reservoir0x/reservoir-sdk'
 import useSWR from 'swr/immutable'
+import { useReservoirClient } from './'
 
 type MarketplaceConfigurationsResponse =
   paths['/collections/{collection}/marketplace-configurations/v1']['get']['responses']['200']['schema']
 
 export default function (
   collectionId: string,
-  chain?: ReservoirChain | null | undefined,
+  chainId?: number,
   enabled: boolean = true
 ) {
+  const client = useReservoirClient()
+  const chain =
+    chainId !== undefined
+      ? client?.chains.find((chain) => chain.id === chainId)
+      : client?.currentChain()
+
   const { data, error } = useSWR<MarketplaceConfigurationsResponse[]>(
     enabled
       ? `${chain?.baseApiUrl}/collections/${collectionId}/marketplace-configurations/v1`
