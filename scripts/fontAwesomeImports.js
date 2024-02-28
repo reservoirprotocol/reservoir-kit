dd882c15c7bcbf230954c945a3e3e5a499dd183f
armando <armandomvillanueva@icloud.com>
2024-02-28 12:44:14 -0800

    feat: fix script

--- scripts/fontAwesomeImports.js
+++ scripts/fontAwesomeImports.js
@@ -2,30 +2,23 @@ const globby = require('globby')
 const fs = require('fs')
 
 async function checkFilesForFontAwesomeIcon() {
-  const files = await globby(['**/*.ts', '**/*.tsx'])
-  let found = false
+  const files = await globby(['packages/ui/src/**/*.ts', 'packages/ui/src/**/*.tsx'])
 
   files.forEach((file) => {
     const content = fs.readFileSync(file, 'utf8')
-    const regex =
-      /import\s*\{\s*(.*?)\s*\}\s*from\s*['"]@fortawesome\/free-solid-svg-icons['"]/g
-    let match
-
-    while ((match = regex.exec(content)) !== null) {
-      const [, icons] = match
-      const individualIcons = icons.split(',').map((icon) => icon.trim())
-      if (!individualIcons.some((icon) => icon.includes('/'))) {
+  const regex = new RegExp('@fortawesome/free-solid-svg-icons/')
+    content.split('\n').forEach((line) => {
+      if (!regex.test(line) && line.includes('@fortawesome/free-solid-svg-icons')) {
+        console.log(line)
         console.error(
           `Found occurrence of { faIcon } from '@fortawesome/free-solid-svg-icons' in ${file}`
         )
-        found = true
+        process.exit(1)
       }
-    }
-  })
+    })
 
-  if (found) {
-    process.exit(1)
-  }
+  })
 }
+
 
 checkFilesForFontAwesomeIcon()
