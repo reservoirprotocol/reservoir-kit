a1bd62b06eb82dc350b5b71703e918b743180b5d
armando <armandomvillanueva@icloud.com>
2024-03-04 14:33:00 -0800

    feat: adjust icons

--- packages/ui/src/img/chains/AstarZkEVMColor.tsx
+++ packages/ui/src/img/chains/AstarZkEVMColor.tsx
@@ -4,8 +4,8 @@ export default () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
-    width="24"
-    height="24"
+    width="auto"
+    height="100%"
     viewBox="0 0 24 24"
     fill="none"
   >
--- packages/ui/src/img/chains/AstarZkEVMDark.tsx
+++ packages/ui/src/img/chains/AstarZkEVMDark.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="auto"
+    height="100%"
     viewBox="0 0 24 24"
     fill="none"
   >
