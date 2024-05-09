ebb669bf04390fabaaf88c501ad0e115775a41a2
pedromcunha <pedro.spinsomewebs@gmail.com>
2024-05-09 14:36:47 -0400

    Fix nonnative crosschain payment token balance fetching

--- packages/sdk/src/utils/paymentTokens.ts
+++ packages/sdk/src/utils/paymentTokens.ts
@@ -613,4 +613,22 @@ export const chainPaymentTokensMap = {
       decimals: 18,
     },
   ],
+
+  // Degen
+  666666666: [
+    {
+      chainId: 666666666,
+      address: zeroAddress,
+      symbol: 'DEGEN',
+      name: 'DEGEN',
+      decimals: 18,
+    },
+    {
+      chainId: 8453,
+      address: '0x4ed4e862860bed51a9570b96d89af5e1b0efefed',
+      symbol: 'DEGEN',
+      name: 'Base DEGEN',
+      decimals: 18,
+    },
+  ],
 } as Record<number, PaymentToken[]>
