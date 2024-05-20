f0e17a25257896799395c91a09ed1c2a1ed94e6d
Jaden <jadendurnford@gmail.com>
2024-05-20 15:29:18 -0400

    feat: adding xai, nebula, degen, and sei testnet icons

---
+++ packages/ui/src/img/chains/XaiIconColor.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_5438_9995)">
+      <g clip-path="url(#clip1_5438_9995)">
+        <path
+          d="M0.0800781 11.96C0.0800781 18.5653 5.43475 23.92 12.0401 23.92C18.6454 23.92 24.0001 18.5653 24.0001 11.96C24.0001 5.35468 18.6454 0 12.0401 0C5.43475 0 0.0800781 5.35468 0.0800781 11.96Z"
+          fill="#F30019"
+        />
+        <path
+          d="M12.0392 9.30327L15.6965 15.483H8.38269L12.04 9.30327M12.04 4.56091L4.04004 18.0791H20.04L12.0392 4.56091H12.0403H12.04Z"
+          fill="white"
+        />
+      </g>
+    </g>
+    <defs>
+      <clipPath id="clip0_5438_9995">
+        <rect width="24" height="24" fill="white" />
+      </clipPath>
+      <clipPath id="clip1_5438_9995">
+        <rect width="24" height="24" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+)
--- packages/ui/src/img/chains/XaiIconLight.tsx
+++ packages/ui/src/img/chains/XaiIconLight.tsx
@@ -2,15 +2,24 @@ import React from 'react'
 
 export default () => (
   <svg
-    width="auto"
-    height="100%"
-    viewBox="0 0 15 24"
+    width="25"
+    height="24"
+    viewBox="0 0 25 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      d="M14.2031 12.2344L7.125 0L0 12.2344L7.125 16.5938L14.2031 12.2344ZM7.125 18L0 13.6406L7.125 24L14.25 13.6406L7.125 18Z"
-      fill="#ECEDEE"
-    />
+    <g clip-path="url(#clip0_5438_9997)">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M12.25 23.92C5.64471 23.92 0.290039 18.5653 0.290039 11.96C0.290039 5.35468 5.64471 0 12.25 0C18.8554 0 24.21 5.35468 24.21 11.96C24.21 18.5653 18.8554 23.92 12.25 23.92ZM12.2496 9.30401L8.59265 15.483H15.9065L12.2496 9.30401ZM12.2496 4.56166L12.25 4.56091H12.2491L12.2496 4.56166ZM4.25 18.0791L12.2496 4.56166L20.25 18.0791H4.25Z"
+        fill="#11181C"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_5438_9997">
+        <rect width="24" height="24" fill="white" transform="translate(0.25)" />
+      </clipPath>
+    </defs>
   </svg>
 )
