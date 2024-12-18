802db6b74003c31aecf66c78127c9feb237b385e
pedromcunha <pedro.spinsomewebs@gmail.com>
2024-12-18 13:11:42 -0500

    Fix order for wrappedContracts

--- packages/ui/src/constants/wrappedContractNames.ts
+++ packages/ui/src/constants/wrappedContractNames.ts
@@ -48,8 +48,8 @@ const wrappedContractNames: Record<number, string> = {
   33111: 'WAPE', // apechain
   360: 'WETH', // shape
   11011: 'WETH', // shape sepolia
-  1946: 'WETH', // minato
   11124: 'WETH', // abstract testnet
+  1946: 'WETH', // minato
   2911: 'WTOPIA', // hychain
   29112: 'WTOPIA', // hychain testnet
   747: 'WFLOW', // flow
