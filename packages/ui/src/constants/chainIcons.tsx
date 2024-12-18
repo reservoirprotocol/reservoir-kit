0e116e52534b624b466746e99c68126f9eed18d8
pedromcunha <pedro.spinsomewebs@gmail.com>
2024-12-18 13:01:39 -0500

    Add back minato icons

--- packages/ui/src/constants/chainIcons.tsx
+++ packages/ui/src/constants/chainIcons.tsx
@@ -109,6 +109,9 @@ import ShapeIconColor from '../img/chains/ShapeIconColor'
 import AbstractIconLight from '../img/chains/AbstractIconLight'
 import AbstractIconDark from '../img/chains/AbstractIconDark'
 import AbstractIconColor from '../img/chains/AbstractIconColor'
+import MinatoIconLight from '../img/chains/MinatoIconLight'
+import MinatoIconDark from '../img/chains/MinatoIconDark'
+import MinatoIconColor from '../img/chains/MinatoIconColor'
 import HychainIconLight from '../img/chains/HychainIconLight'
 import HychainIconDark from '../img/chains/HychainIconDark'
 import HychainIconColor from '../img/chains/HychainIconColor'
@@ -379,6 +382,11 @@ const chainIcons = {
     light: <AbstractIconLight />,
     dark: <AbstractIconDark />,
     color: <AbstractIconColor />,
+  },
+  [reservoirChains.minato.id]: {
+    light: <MinatoIconLight />,
+    dark: <MinatoIconDark />,
+    color: <MinatoIconColor />,
   },
   [reservoirChains.hychain.id]: {
     light: <HychainIconLight />,
